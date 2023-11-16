c0ef091d81963160703ac713a6c996f714227b28
darko <ki14.skakun@gmail.com>
2023-11-16 17:36:17 +0200

    rendering item and categories on the page

--- item/models.py
+++ item/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -13,12 +14,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Item(models.Model):
-    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
