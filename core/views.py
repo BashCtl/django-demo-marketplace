147c3d1283aa5afb6aa997ae01e380795bccf881
darko <ki14.skakun@gmail.com>
2023-11-17 12:01:14 +0200

    user signup

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
+from .forms import SignupForm
 
 # Create your views here.
+
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -10,5 +12,19 @@ def index(request):
 
     return render(request, 'core/index.html', context)
 
+
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = SignupForm()
+
+    context = {'form': form}
+    return render(request, 'core/signup.html', context)
