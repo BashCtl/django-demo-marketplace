9ced0e66bc93bd3594a6cb81c9a232de6492fff0
darko <ki14.skakun@gmail.com>
2023-11-18 10:37:04 +0200

    edit item

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
@@ -30,6 +30,23 @@ def new_item(request):
         form = NewItemForm()
 
     context = {'form': form, 'title': 'New item'}
+    return render(request, 'item/form.html', context)
+
+
+@login_required
+def edit_item(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.pk)
+    else:
+        form = EditItemForm(instance=item)
+
+    context = {'form': form, 'title': 'Edit item'}
     return render(request, 'item/form.html', context)
 
 
